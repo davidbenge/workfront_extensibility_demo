d4864509e7e2cc91042790a8602c39556f04af37
David Benge <dbenge@adobe.com>
2025-03-25 16:04:22 -0700

    added serverless function to the project

--- src/workfront-ui-1/actions/getdata/index.js
+++ src/workfront-ui-1/actions/getdata/index.js
@@ -11,6 +11,7 @@
  *   - Remove the Authorization header from the array passed in checkMissingRequestInputs
  *   - The two steps above imply that every client knowing the URL to this deployed action will be able to invoke it without any authentication and authorization checks against Adobe Identity Management System
  *   - Make sure to validate these changes against your security requirements before deploying the action
+ *   - you can find the url to this action with command `aio app get-url
  */
 
 
